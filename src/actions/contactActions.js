c58febc520f934c57d499e471cd40117663ab9d5
Mauro <mgaldames@azurian.com>
2018-08-31 09:38:07 -0300

    impment GET request dispatch in action

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,11 +1,20 @@
 // bring types
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from './types';
+import axios from 'axios';
 
-export const getContacts = () => {
+/**
+ * every call request is made within the actions
+ */
+
+// react thunk allow to do the follow
+export const getContacts = () => async dispatch => {
   // here is where the call to GET API is perform
-  return {
-    type: GET_CONTACTS
-  };
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch({
+      type: GET_CONTACTS,
+      payload: response.data
+  })
+  
 };
 
 export const deleteContact = id => {
