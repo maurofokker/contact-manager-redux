9364dd8693410a02e2c91cd54e06be17af676dc9
maurofokker <mgaldames@gmail.com>
2018-08-30 23:09:16 -0300

    connect components and dispatch GET_CONTACTS action

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+// allow to connect with the contactReducer
+import { connect } from 'react-redux';
+// everithing from redux state (actions) is put into props
+import PropTypes from 'prop-types';
+import { GET_CONTACTS } from '../../actions/types';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
-      }
-    ]
-  };
+  componentDidMount() {
+    // this will put contacts into the props
+    this.props.getContacts();
+  }
 
   render() {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="display-4 mb-2">
@@ -40,4 +27,25 @@ class Contacts extends Component {
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  getContacts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  contacts: state.contact.contacts
+});
+// to access in props -> this.props.contacts
+
+const mapDispatchToProps = dispatch => ({
+  // name of actions
+  getContacts: () => dispatch({ type: GET_CONTACTS })
+});
+
+// in order to use connect it must wrap component
+// first parameter everything we want to map from the redux state to the props in the component
+// second parameter anything we want to dispatch (any action to dispatch)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contacts);
